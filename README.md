b21298bf41f47fc25d8ff81ef5e700a785310d00
satish.muddam <satish.muddam@appdynamics.com>
2019-01-18 08:22:10 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # hello-world-teamcity
-Test teamcity
---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * @author: {Vishaka Sekar} on {1/17/19}
+ */
+public class HelloWorld {
+
+    public static void main(String args[]) {
+        System.out.println("hello");
+    }
+}
